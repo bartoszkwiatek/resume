59ecc375813affa63d2671a3bc0f0e9470c96bd9
kwiaciu <bkwiatek3@gmail.com>
2020-07-06 18:36:43 +0200

    Updates

---
+++ precache-manifest.fd4660de1c73e498867c8d0fb332baeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2955747b87ece4307e39802926f742e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21627ecf68ea3e663e67",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "45049b3418160f956488",
+    "url": "/static/js/2.e3a1641a.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/static/js/2.e3a1641a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21627ecf68ea3e663e67",
+    "url": "/static/js/main.cf8be459.chunk.js"
+  },
+  {
+    "revision": "79674530e8b32cc45c00",
+    "url": "/static/js/runtime-main.ddf34761.js"
+  }
+]);
