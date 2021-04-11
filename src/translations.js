c8f7c5aa90d3d64e0c5b498505a94343bfe3fb80
kwiaciu <bkwiatek3@gmail.com>
2021-04-11 17:23:02 +0200

    created file with transaltions for ui

---
+++ src/translations.js
@@ -0,0 +1,29 @@
+export const translations = {
+  "data": {
+    "en": {
+      "resume": "Resume",
+      "darkmode": "Dark mode",
+      "about": "About me",
+      "email": "E-mail",
+      "phone": "Phone",
+      "languages": "Languages",
+      "skills": "Skills",
+      "currentSkill": "Current skill levels",
+      "projects": "Projects",
+      "education": "Education",
+      "experience": "Experience"
+    },
+    "pl": {
+      "resume": "CV",
+      "darkmode": "Tryb ciemny",
+      "about": "O mnie",
+      "email": "E-mail",
+      "phone": "Telefon",
+      "languages": "Języki",
+      "skills": "Umiejętności",
+      "currentSkill": "Obecnie",
+      "projects": "Projekty",
+      "education": "Wykształcenie",
+      "experience": "Doświadczenie"
+  }
+}
