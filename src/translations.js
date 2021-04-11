5eb81344b53cfc0cd00d88c32d286be0bab8db0d
kwiaciu <bkwiatek3@gmail.com>
2021-04-11 17:23:32 +0200

    created file with transaltions for ui

--- src/translations.js
+++ src/translations.js
@@ -1,29 +1,28 @@
 export const translations = {
-  "data": {
-    "en": {
-      "resume": "Resume",
-      "darkmode": "Dark mode",
-      "about": "About me",
-      "email": "E-mail",
-      "phone": "Phone",
-      "languages": "Languages",
-      "skills": "Skills",
-      "currentSkill": "Current skill levels",
-      "projects": "Projects",
-      "education": "Education",
-      "experience": "Experience"
-    },
-    "pl": {
-      "resume": "CV",
-      "darkmode": "Tryb ciemny",
-      "about": "O mnie",
-      "email": "E-mail",
-      "phone": "Telefon",
-      "languages": "Języki",
-      "skills": "Umiejętności",
-      "currentSkill": "Obecnie",
-      "projects": "Projekty",
-      "education": "Wykształcenie",
-      "experience": "Doświadczenie"
-  }
+  en: {
+    resume: 'Resume',
+    darkmode: 'Dark mode',
+    about: 'About me',
+    email: 'E-mail',
+    phone: 'Phone',
+    languages: 'Languages',
+    skills: 'Skills',
+    currentSkill: 'Current skill levels',
+    projects: 'Projects',
+    education: 'Education',
+    experience: 'Experience',
+  },
+  pl: {
+    resume: 'CV',
+    darkmode: 'Tryb ciemny',
+    about: 'O mnie',
+    email: 'E-mail',
+    phone: 'Telefon',
+    languages: 'Języki',
+    skills: 'Umiejętności',
+    currentSkill: 'Obecnie',
+    projects: 'Projekty',
+    education: 'Wykształcenie',
+    experience: 'Doświadczenie',
+  },
 }
